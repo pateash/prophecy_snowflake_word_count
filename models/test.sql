f345adc1acfd7364d6a47c59d05c49a97476fd49
ashishpatel0720@gmail.com <ashishpatel0720@gmail.com>
2024-05-08 09:30:29 +0000

    Cloning prophecy project main branch

---
+++ models/test.sql
@@ -0,0 +1,36 @@
+WITH README AS (
+
+  SELECT * 
+  
+  FROM {{ source('ASHISH.PUBLIC', 'README') }}
+
+),
+
+tokenize AS (
+
+  SELECT 
+    LINE AS LINE,
+    CASE
+      WHEN LINE IS NOT NULL
+        THEN SPLIT(REGEXP_REPLACE(line, '\\s+', ' '), ' ')
+      ELSE ARRAY_CONSTRUCT()
+    END AS words
+  
+  FROM README AS in0
+
+),
+
+word_count AS (
+
+  SELECT 
+    LINE AS LINE,
+    WORDS AS WORDS,
+    ARRAY_SIZE(WORDS) AS words_count
+  
+  FROM tokenize AS in0
+
+)
+
+SELECT *
+
+FROM word_count
